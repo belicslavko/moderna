ea91e2e0d26f461e14557a4a9f7c5860c0dfbb79
Belic Slavko <belicslavko@yahoo.com>
2018-01-04 16:25:49 +0100

    Theme init

---
+++ wp-content/themes/moderna/footer.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the "site-content" div and all content after.
+ *
+ * @package WordPress
+ * @subpackage Moderna
+ * @since Moderna 1.0
+ */
+?>
